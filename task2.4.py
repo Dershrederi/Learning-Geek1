70ab17d94bc70b5b32ec7f196226a5359995867f
Dershrederi <112941274+Dershrederi@users.noreply.github.com>
2022-09-12 15:46:36 +0300

    Add files via upload

---
+++ task2.4.py
@@ -0,0 +1,12 @@
+# 5. Реализуйте алгоритм перемешивания списка.
+
+from random import random
+
+
+list = ['true', '12', 'react', 'love', 'work']
+print(list)
+import random
+random.shuffle(list)
+print(list)
+
+
